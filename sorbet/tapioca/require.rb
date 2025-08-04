8c837fba0cc4adef69491911ebbe28c7597fcca5
Franklin Hu <franklin@thisisfranklin.com>
2025-08-04 08:43:30 -0700

    tapioca require

--- sorbet/tapioca/require.rb
+++ sorbet/tapioca/require.rb
@@ -1,4 +1,19 @@
 # typed: true
 # frozen_string_literal: true
 
-# Add your extra requires here (`bin/tapioca require` can be used to bootstrap this list)
+require "better_html"
+require "better_html/parser"
+require "erb"
+require "fileutils"
+require "minitest/autorun"
+require "pathname"
+require "psych"
+require "rails"
+require "rubygems/user_interaction"
+require "sorbet-runtime"
+require "tapioca/dsl"
+require "tapioca/helpers/test/content"
+require "tapioca/helpers/test/dsl_compiler"
+require "tapioca/internal"
+require "view_component"
+require "view_component/slotable"
